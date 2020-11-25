f108a6eff2c04f244518f56d011bdd6c4ed3f1a2
WebDev Brazil <webdevbrazil@gmail.com>
2020-11-25 10:54:32 -0300

    Commit inicial

---
+++ precache-manifest.bad693c6d30e97385101c57b1aee420d.js
@@ -0,0 +1,334 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b08ad333383bdacae33955042a73f8f",
+    "url": "/assets/fonts/font-awesome/css/all.css"
+  },
+  {
+    "revision": "76cb46c10b6c0293433b371bae2414b2",
+    "url": "/assets/fonts/font-awesome/css/all.min.css"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/assets/fonts/font-awesome/webfonts/fa-brands-400.eot"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/assets/fonts/font-awesome/webfonts/fa-brands-400.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/assets/fonts/font-awesome/webfonts/fa-brands-400.ttf"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/assets/fonts/font-awesome/webfonts/fa-brands-400.woff"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/assets/fonts/font-awesome/webfonts/fa-brands-400.woff2"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/assets/fonts/font-awesome/webfonts/fa-regular-400.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/assets/fonts/font-awesome/webfonts/fa-regular-400.svg"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/assets/fonts/font-awesome/webfonts/fa-regular-400.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/assets/fonts/font-awesome/webfonts/fa-regular-400.woff"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/assets/fonts/font-awesome/webfonts/fa-regular-400.woff2"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/assets/fonts/font-awesome/webfonts/fa-solid-900.eot"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/assets/fonts/font-awesome/webfonts/fa-solid-900.svg"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/assets/fonts/font-awesome/webfonts/fa-solid-900.ttf"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/assets/fonts/font-awesome/webfonts/fa-solid-900.woff"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/assets/fonts/font-awesome/webfonts/fa-solid-900.woff2"
+  },
+  {
+    "revision": "2ed3cab987736a2244537a3f8a4d6381",
+    "url": "/assets/img/logo.png"
+  },
+  {
+    "revision": "2f894a4a83202d945948a32e6133c43d",
+    "url": "/assets/img/logo1.png"
+  },
+  {
+    "revision": "e538ef3c84d2dd684e006fe6bfa1adb3",
+    "url": "/assets/img/separators/arrow-negative.svg"
+  },
+  {
+    "revision": "047d6d3ca586023ed533e98314567fac",
+    "url": "/assets/img/separators/arrow.svg"
+  },
+  {
+    "revision": "3c1fef12806aabbaef0cf11feea8f960",
+    "url": "/assets/img/separators/book-negative.svg"
+  },
+  {
+    "revision": "fc5458cf26ca2c9f576e669aaec8a2c4",
+    "url": "/assets/img/separators/book.svg"
+  },
+  {
+    "revision": "50db5efb85067a17069db2eb71f5a583",
+    "url": "/assets/img/separators/clouds-negative.svg"
+  },
+  {
+    "revision": "81ca520c7d5db697290cd6a7a6a8bbcf",
+    "url": "/assets/img/separators/clouds.svg"
+  },
+  {
+    "revision": "ba1140a80d47f9c79374a7eb1d75d90a",
+    "url": "/assets/img/separators/curve-asymmetrical-negative.svg"
+  },
+  {
+    "revision": "c17135654bc27c68d5f3723c66afd8a1",
+    "url": "/assets/img/separators/curve-asymmetrical.svg"
+  },
+  {
+    "revision": "c203c6283885e9a5688c4b71a551abe3",
+    "url": "/assets/img/separators/curve-negative.svg"
+  },
+  {
+    "revision": "563fcdbe95b1ee9254767b8c52603953",
+    "url": "/assets/img/separators/curve.svg"
+  },
+  {
+    "revision": "1c0bfc917912582ef15f1647beec00e8",
+    "url": "/assets/img/separators/drops-negative.svg"
+  },
+  {
+    "revision": "cecd4a56e57ce6fbb7e1f4cf527ddb0d",
+    "url": "/assets/img/separators/drops.svg"
+  },
+  {
+    "revision": "d01a05b415c865149c0f914c4cb905a3",
+    "url": "/assets/img/separators/mesmerize/1.wave-and-line-negative.svg"
+  },
+  {
+    "revision": "18657cb08e056f823aba71e9358e9df1",
+    "url": "/assets/img/separators/mesmerize/1.wave-and-line.svg"
+  },
+  {
+    "revision": "965c60cf591f9bb7535adabf38aab8ed",
+    "url": "/assets/img/separators/mesmerize/3.waves-noCentric-negative.svg"
+  },
+  {
+    "revision": "a81e0a80c7fe290e553e89d0e7f026a1",
+    "url": "/assets/img/separators/mesmerize/3.waves-noCentric.svg"
+  },
+  {
+    "revision": "ec2e0df208a57801a5af2778a31fd8b4",
+    "url": "/assets/img/separators/mesmerize/4.clouds.svg"
+  },
+  {
+    "revision": "59b1e8cd04253336ffcbe99775740399",
+    "url": "/assets/img/separators/mesmerize/5.triple-waves-3-negative.svg"
+  },
+  {
+    "revision": "c2c9e5a61ecaca13ea90f7dea729ee6a",
+    "url": "/assets/img/separators/mesmerize/5.triple-waves-3.svg"
+  },
+  {
+    "revision": "8efdd2bc011ad003473665a44419355b",
+    "url": "/assets/img/separators/mesmerize/6.triple-waves-2-negative.svg"
+  },
+  {
+    "revision": "7f5a23aab6f4384857297fcfe17de46e",
+    "url": "/assets/img/separators/mesmerize/6.triple-waves-2.svg"
+  },
+  {
+    "revision": "2a048a7464b4d98af90659719bd4ae9d",
+    "url": "/assets/img/separators/mesmerize/7.stright-angles-1-negative.svg"
+  },
+  {
+    "revision": "45c40c69da08903eb31e27911b72c1e5",
+    "url": "/assets/img/separators/mesmerize/7.stright-angles-1.svg"
+  },
+  {
+    "revision": "5961423b795100a42c3b3c66e6bd19fc",
+    "url": "/assets/img/separators/mesmerize/8.stright-angles-2-negative.svg"
+  },
+  {
+    "revision": "92816774dbab40541a3e9189e1350564",
+    "url": "/assets/img/separators/mesmerize/8.stright-angles-2.svg"
+  },
+  {
+    "revision": "1dd6e8de42656b9c0eee5dc622f092ee",
+    "url": "/assets/img/separators/mountains.svg"
+  },
+  {
+    "revision": "3740d56fd7f060f405cc649325fae226",
+    "url": "/assets/img/separators/opacity-fan.svg"
+  },
+  {
+    "revision": "a242cc29349e3ab3a27d34d13c8f1dfa",
+    "url": "/assets/img/separators/opacity-tilt.svg"
+  },
+  {
+    "revision": "78ce0304076b8ec54f2418db8ba3a5a6",
+    "url": "/assets/img/separators/pyramids-negative.svg"
+  },
+  {
+    "revision": "d14ff28da34c69cebf3bbf41d4e6f373",
+    "url": "/assets/img/separators/pyramids.svg"
+  },
+  {
+    "revision": "99eaea0d7e5cdbabdd5dcb1d7824dc3b",
+    "url": "/assets/img/separators/split-negative.svg"
+  },
+  {
+    "revision": "8faa5c8e78a59791be0bc31d58eb51e0",
+    "url": "/assets/img/separators/split.svg"
+  },
+  {
+    "revision": "eb6807fc02cd0899e72e87409cf8b614",
+    "url": "/assets/img/separators/tilt-flipped.svg"
+  },
+  {
+    "revision": "aada1aac3d9788481fe36ec969b1f1d0",
+    "url": "/assets/img/separators/tilt.svg"
+  },
+  {
+    "revision": "022f84a771ea5c686e0d460f875c3c4d",
+    "url": "/assets/img/separators/triangle-asymmetrical-negative.svg"
+  },
+  {
+    "revision": "574a8e44e55f7274892dbf58bdddb28b",
+    "url": "/assets/img/separators/triangle-asymmetrical.svg"
+  },
+  {
+    "revision": "65cd0da5c7849e0663e9bc7f28f52e3f",
+    "url": "/assets/img/separators/triangle-negative.svg"
+  },
+  {
+    "revision": "aed81dccb2c539de183caa3c200a0936",
+    "url": "/assets/img/separators/triangle.svg"
+  },
+  {
+    "revision": "97231228d4d7ec1865db9db16ccbd517",
+    "url": "/assets/img/separators/wave-brush.svg"
+  },
+  {
+    "revision": "c41141047ea21fb953f1a639f7dd50af",
+    "url": "/assets/img/separators/waves-negative.svg"
+  },
+  {
+    "revision": "03fe24d6888d26f41f009860b6a3b8cc",
+    "url": "/assets/img/separators/waves-pattern.svg"
+  },
+  {
+    "revision": "28234183438b3acdb045990687c1eb8e",
+    "url": "/assets/img/separators/waves.svg"
+  },
+  {
+    "revision": "439e9db25dfa5c5d028dcb12723a10fc",
+    "url": "/assets/img/separators/zigzag.svg"
+  },
+  {
+    "revision": "5504cda29e2d45ad2f9f6f7271cf0295",
+    "url": "/assets/img/shapes/circles.png"
+  },
+  {
+    "revision": "e81dbb4c7482dfcbc648",
+    "url": "/css/app.cbd74ce5.css"
+  },
+  {
+    "revision": "6dce3db41f03552f4f26",
+    "url": "/css/module_app.33758118.css"
+  },
+  {
+    "revision": "d746fb8980bc4d85f616",
+    "url": "/css/module_user.9878c1ba.css"
+  },
+  {
+    "revision": "36fd4239cf4a8ed8ffed4ad4b2e05b3d",
+    "url": "/deleteModal.html"
+  },
+  {
+    "revision": "58bdf6615a7324ffa93afb835cbc3e38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e81dbb4c7482dfcbc648",
+    "url": "/js/app.e0aa2d3c.js"
+  },
+  {
+    "revision": "5ad39f40623494935a1b",
+    "url": "/js/chunk-vendors.639a5797.js"
+  },
+  {
+    "revision": "6dce3db41f03552f4f26",
+    "url": "/js/module_app.be68efed.js"
+  },
+  {
+    "revision": "3e5a74d30913b9898357",
+    "url": "/js/module_main.0eeea2b9.js"
+  },
+  {
+    "revision": "d746fb8980bc4d85f616",
+    "url": "/js/module_user.2f0ae655.js"
+  },
+  {
+    "revision": "950b24c88abb1ab13601f700c0c9619a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "c04857f75a8b2da8d0e4e3a68a1bc83e",
+    "url": "/medias/headlines/bg1.jpg"
+  },
+  {
+    "revision": "08a57e9d69b8639b302256d9ff3ca8a3",
+    "url": "/medias/headlines/bg2.jpg"
+  },
+  {
+    "revision": "e0ef00dc8fe653235c2192be381dd844",
+    "url": "/medias/headlines/bg3.jpg"
+  },
+  {
+    "revision": "1ce816017ba9f5ef7ad5c35e5e384d3f",
+    "url": "/medias/headlines/bg4.jpg"
+  },
+  {
+    "revision": "aa267be0e53e5092c5fa776874b58b2e",
+    "url": "/modal.html"
+  },
+  {
+    "revision": "df9afabb604ecbc08c6edab380c15933",
+    "url": "/parallax_percent.html"
+  },
+  {
+    "revision": "abc9daabbe7ed5d992bfe2539aacdc42",
+    "url": "/parallax_pixels.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "99f3c7e2678d33541136dde287a7f3d7",
+    "url": "/toggle.html"
+  }
+]);
